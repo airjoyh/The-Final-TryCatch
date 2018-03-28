dd1b6c9cb4cae694440560f7e4f4dc14644e3145
leesungwon <leesungwon@DESKTOP-6OJ71V6>
2018-03-28 13:27:44 +0900

    [다정]page 5개씩 보이기

--- src/main/java/kr/co/trycatch/domain/user/PageMaker.java
+++ src/main/java/kr/co/trycatch/domain/user/PageMaker.java
@@ -19,7 +19,7 @@ public class PageMaker {
 	private int endPage; // 10 ¶Ç´Â 13
 	private boolean prev; // '<<'
 	private boolean next; // '>>'
-	private int displayPageNum = 10;// 1 2 3 4 5 6 7 8 9 10
+	private int displayPageNum = 5;// 1 2 3 4 5 6 7 8 9 10
 									// 11 12 13 14 15 16 17 18 19 20
 
 	private Criteria cri;
